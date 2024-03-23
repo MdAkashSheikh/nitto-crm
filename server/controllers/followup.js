d43b2205152e950f029e814b008fd26dc182d81d
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-03-24 00:09:48 +0600

    route and controlller add

--- server/controllers/followup.js
+++ server/controllers/followup.js
@@ -32,7 +32,32 @@ const postFollow = async(req, res) => {
     }
 }
 
+const editFollow = async(req, res) => {
+    const id = req.params.id;
+    const zone = req.body.zone;
+    const dataSource = req.body.dataSource;
+    const category = req.body.category;
+    const name = req.body.name;
+    const address = req.body.address;
+    const phone = req.body.phone;
+    const email = req.body.email;
+    const whatsapp = req.body.whatsapp;
+    const details = req.body.details;
+
+    try {
+        const oneData = await customerInfoSc.findByIdAndUpdate(id,  {
+            zone, dataSource, category, name, address, phone, email, whatsapp, details
+        })
+
+        res.status(200).send(oneData);
+
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+}
+
 module.exports = {
     postFollow,
-
+    editFollow,
 }
